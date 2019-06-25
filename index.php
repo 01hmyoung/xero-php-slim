bf167cae905e9c71aa332b4056940e7ef47d8d0a
Michael Young <myoung123@hotmail.co.uk>
2019-06-25 09:36:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,16 +18,6 @@ $app->get('/invoices/{Identifier}', function ($request, $response, array $args) 
 
     $companyIdentifier = $args['Identifier'];
 
-        $config =   [
-            'oauth' => [
-                'callback' => 'localhost',
-                'consumer_key' => 'HPMBEXWBFW1OBI1WJF0IDDMI5QCBYG',
-                'consumer_secret' => 'BUDWWNAQH1HOQJ7HFD1JFONWML4IXK',
-                'rsa_private_key' => 'file://certs/privatekey.pem',
-            ],
-        ];
-
-
     $xero = new PrivateApplication($config);
 
     print_r($xero->load('Accounting\\Invoice')->page(1)->execute());
